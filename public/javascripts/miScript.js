85f7e4d4936c756fafc57d99852bf2638002d67f
Jorge Salgado <salgado.jorge@dreyfous.local>
2016-09-22 15:52:08 -0400

    casi final

--- public/javascripts/miScript.js
+++ public/javascripts/miScript.js
@@ -4,45 +4,7 @@ $( init );
 
 
 function init() {
-    
-    //scale
-    
-    var $el = $("#very-specific-design");
-var elHeight = $el.outerHeight();
-var elWidth = $el.outerWidth();
-
-var $wrapper = $("#content");
-
-$wrapper.resizable({
-  resize: doResize
-});
-
-function doResize(event, ui) {
-  
-  var scale, origin;
-    
-  scale = Math.min(
-    ui.size.width / elWidth,    
-    ui.size.height / elHeight
-  );
-  
-  $el.css({
-    transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
-  });
-  
-}
-
-var starterData = { 
-  size: {
-    width: $wrapper.width(),
-    height: $wrapper.height()
-  }
-}
-doResize(null, starterData);
-    
-    
-    //scale final
-    
+     
 
   // Hide the success message
   $('#successMessage').hide();
@@ -59,8 +21,8 @@ doResize(null, starterData);
   $('#cardSlots').html( '' );
 
   // Create the pile of shuffled cards
-  var numbers = [ 1, 2, 3];
-  var fotos = [ 'asteroide', 'volcan', 'clima'];
+    var numbers = [ 1, 2, 3];
+    var fotos = [ 'asteroide', 'volcan', 'clima'];
     var numeroRandom = Math.random() - .5;
 
   numbers.sort( function() { return numeroRandom } );
@@ -68,24 +30,27 @@ doResize(null, starterData);
     
 
   for ( var i=0; i<3; i++ ) {
-      $('<img src' + numbers[i] + '>')
-    $('<div>' + '</div>').data( 'number', numbers[i] ).attr( 'id', 'card' +numbers[i] ).appendTo( '#cardPile' ).draggable( {
+ $('<div>' + '</div>').data( 'number', numbers[i] ).attr( 'id', 'card' +numbers[i] ).appendTo('#cardPile' ).draggable( {
       containment: '#content',
       stack: '#cardPile div',
       cursor: 'pointer',
       revert: true
     } )
     .append('<img id="theImg" class="photo" src="../images/' + fotos[i] + '.jpg"/>');
+    
   }
 
   // Create the card slots
+    //$('#cardSlots').append('<img src="../images/rockcardslot.png"/>')
   var words = [ 'Impacto de un asteroide 1', 'Erupcion volcanica 2', 'Cambios climaticos 3'];
   for ( var i=1; i<=3; i++ ) {
     $('<div>' + words[i-1] + '</div>').data( 'number', i ).appendTo( '#cardSlots' ).droppable( {
       accept: '#cardPile div',
       hoverClass: 'hovered',
       drop: handleCardDrop
+        
     } );
+    
   }
 
 }
